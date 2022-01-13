08020df4712f30904982160964fb39c4546f7799
Svetlana Asotikova <asotikova.sv@gmail.com>
2022-01-13 12:29:17 +0300

    Styles upd

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/services">
       <App />
     </BrowserRouter>
   </Provider>,
